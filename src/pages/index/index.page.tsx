4ebaa736acfd0e6bdc79ebb6c151297cf9d6888d
qwqVictor <i@qwq.ren>
2023-03-11 09:24:24 +0800

    feat: last update

--- src/pages/index/index.page.tsx
+++ src/pages/index/index.page.tsx
@@ -5,7 +5,7 @@ import Header from '../../components/header'
 function Page() {
   return (
     <>
-      <p></p>
+      <p className="resume-last-updated">最后更新于 {new Date().toLocaleDateString()}。</p>
       <div className="container">
         <Header />
         <div className="divider">
