e834a11dc9ab9c388ffd874689f9a8d707f54635
qwqVictor <i@qwq.ren>
2023-03-11 10:33:42 +0800

    feat: add floating buttons

---
+++ src/components/floating.tsx
@@ -0,0 +1,20 @@
+import './floating.css'
+
+function Floating() {
+  return (
+    <div className="floating-buttons">
+      <a href='' onClick={() => {
+        document.execCommand("print")
+      }} className="floating-button" style={{backgroundColor: '#0ecaa3'}}>
+        <span className="floating-button-icon"><i className="fa-solid fa-print"></i></span>
+        <span className="floating-button-text">打印本页</span>
+      </a>
+      <a href="resume.pdf" className="floating-button" style={{backgroundColor: '#0e92ca'}}>
+        <span className="floating-button-icon"><i className="fa-solid fa-file"></i></span>
+        <span className="floating-button-text">PDF 简历</span>
+      </a>
+    </div>
+  )
+}
+
+export default Floating
--- src/pages/index/index.page.tsx
+++ src/pages/index/index.page.tsx
@@ -1,11 +1,12 @@
 import './index.page.css'
 import Content from '../../components/content'
 import Header from '../../components/header'
+import Floating from '../../components/floating'
 
 function Page() {
   return (
     <>
-      <p className="resume-last-updated">最后更新于 {new Date().toLocaleDateString()}。</p>
+      <p className="resume-last-updated">最后更新于 {new Date().toLocaleDateString("zh-CN")}。</p>
       <div className="container">
         <Header />
         <div className="divider">
@@ -13,6 +14,7 @@ function Page() {
         </div>
         <Content />
       </div>
+      <Floating />
       <p></p>
     </>
   )
