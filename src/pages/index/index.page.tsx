3f9d2b846daebaa80e487f8786b42da9a1ca1696
qwqVictor <i@qwq.ren>
2023-03-14 11:23:04 +0800

    fix: static last update time

--- src/pages/index/index.page.tsx
+++ src/pages/index/index.page.tsx
@@ -6,7 +6,7 @@ import Floating from '../../components/floating'
 function Page() {
   return (
     <>
-      <p className="resume-last-updated">最后更新于 {new Date().toLocaleDateString("zh-CN")}。</p>
+      <p className="resume-last-updated">最后更新于 __RESUME_LAST_UPDATED_BUILD_TIME。</p>
       <div className="container">
         <Header />
         <div className="divider">
--- vite.config.ts
+++ vite.config.ts
@@ -7,4 +7,7 @@ export default defineConfig({
   plugins: [react(), ssr({
     prerender: true
   })],
+  define: {
+    __RESUME_LAST_UPDATED_BUILD_TIME: new Date().toLocaleDateString("zh-CN")
+  }
 })
